343576032d833ce67aa4c5dafe1cf56ba3165e75
ilmare-cbk <cbk104325@gmail.com>
2017-05-27 16:06:42 +0900

    main에서 profile로 넘어가기

--- app/src/main/java/com/midaschallenge/midasitchallenge2017/MainActivity.java
+++ app/src/main/java/com/midaschallenge/midasitchallenge2017/MainActivity.java
@@ -1,12 +1,18 @@
 package com.midaschallenge.midasitchallenge2017;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -16,11 +22,12 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener{
 
     private RecyclerView rv_talentDonationList;
     private RecyclerView.LayoutManager rv_layoutManager;
     private TalentRecyclerViewAdapter talentRecyclerViewAdapter;
+    private ImageButton mainProfileBtn;
 
     private ArrayList<TalentDonationDTO> talentDonationDTOs;
     @Override
@@ -31,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init() {
+
+        mainProfileBtn = (ImageButton) findViewById(R.id.main_profile_btn);
+        mainProfileBtn.setOnTouchListener(this);
 
         rv_layoutManager = new LinearLayoutManager(this);
         rv_talentDonationList = (RecyclerView) findViewById(R.id.rv_main_talent_donation);
@@ -62,4 +72,17 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int action = event.getAction();
+        switch (v.getId()) {
+            case R.id.main_profile_btn:
+                if (action == MotionEvent.ACTION_UP) {
+                    Intent intent = new Intent(this, ProfileActivity.class);
+                    startActivity(intent);
+                }
+                return true;
+        }
+        return false;
+    }
 }
