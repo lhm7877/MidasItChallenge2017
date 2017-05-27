203e5fcdf8e8f68a9002195a2b8910ac3bf095d4
lhm7877 <lhm7877@naver.com>
2017-05-27 16:08:19 +0900

    ㅁ

--- app/src/main/java/com/midaschallenge/midasitchallenge2017/MainActivity.java
+++ app/src/main/java/com/midaschallenge/midasitchallenge2017/MainActivity.java
@@ -1,12 +1,17 @@
 package com.midaschallenge.midasitchallenge2017;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -16,7 +21,9 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private ImageButton main_profile_btn;
+    private FloatingActionButton fbtn_add_request_talent_donation;
 
     private RecyclerView rv_talentDonationList;
     private RecyclerView.LayoutManager rv_layoutManager;
@@ -32,18 +39,21 @@ public class MainActivity extends AppCompatActivity {
 
     private void init() {
 
+        main_profile_btn = (ImageButton) findViewById(R.id.main_profile_btn);
+        fbtn_add_request_talent_donation = (FloatingActionButton) findViewById(R.id.fbtn_add_request_talent_donation);
+        fbtn_add_request_talent_donation.setOnClickListener(this);
+
         rv_layoutManager = new LinearLayoutManager(this);
         rv_talentDonationList = (RecyclerView) findViewById(R.id.rv_main_talent_donation);
         rv_talentDonationList.setLayoutManager(rv_layoutManager);
 
-
-        callSticker();
+        callTalentDonationList();
         //TODO: talentDonationDTOs 받아오기
         talentRecyclerViewAdapter = new TalentRecyclerViewAdapter(this,talentDonationDTOs);
         rv_talentDonationList.setAdapter(talentRecyclerViewAdapter);
     }
 
-    private void callSticker(){
+    private void callTalentDonationList(){
         TalentDonationService talentDonationService = TalentDonationService.retrofit.create(TalentDonationService.class);
         Call<ArrayList<TalentDonationDTO>> call = talentDonationService.talentDonationList();
         call.enqueue(new Callback<ArrayList<TalentDonationDTO>>() {
@@ -62,4 +72,12 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.fbtn_add_request_talent_donation :
+                Intent intent = new Intent();
+
+        }
+    }
 }
